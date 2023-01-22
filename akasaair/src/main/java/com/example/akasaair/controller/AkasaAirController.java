78e1acc5b086ccbd9330d3cc90202d59d861af79
sekharvirat <sekharreddy1515@gmail.com>
2023-01-22 13:13:18 +0530

    Added Rate Limiting

--- akasaair/src/main/java/com/example/akasaair/controller/AkasaAirController.java
+++ akasaair/src/main/java/com/example/akasaair/controller/AkasaAirController.java
@@ -6,6 +6,7 @@ import com.example.akasaair.model.FlightValues;
 import com.example.akasaair.service.FlightService;
 import com.example.akasaair.service.JwtTokenProvider;
 import com.example.akasaair.utilities.JwtTokenResponse;
+import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,61 +17,74 @@ import org.springframework.web.bind.annotation.*;
 public class AkasaAirController {
 
     @Autowired
+    private final RateLimiter rateLimiter;
+    @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
     @Autowired
     private FlightService flightService;
 
-    @GetMapping("/")
-    public String defaultMethod() {
-        return "Hi";
+    @Autowired
+    public AkasaAirController(RateLimiter rateLimiter, JwtTokenProvider jwtTokenProvider, FlightService flightService) {
+        this.rateLimiter = rateLimiter;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.flightService = flightService;
     }
+
 
     @GetMapping("/api/auth")
-    public ResponseEntity<JwtTokenResponse> generateToken() {
-        String jwt = jwtTokenProvider.createToken("username");
-        return ResponseEntity.ok(new JwtTokenResponse(jwt));
+    public ResponseEntity<?> generateToken() {
+        if (rateLimiter.tryAcquire()) {
+            String jwt = jwtTokenProvider.createToken("username");
+            return ResponseEntity.ok(new JwtTokenResponse(jwt));
+        }
+        return new ResponseEntity<>("Number of requests exceeded", HttpStatus.OK);
     }
 
-    @GetMapping("/validateToken")
-    public ResponseEntity<String> validateToken() {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VybmFtZSIsImlhdCI6MTY3NDMwMjEwMCwiZXhwIjoxNjc0MzM4MDkwfQ.hH82yqvUHdGb087lw2hxhWE_xXBkZN2UgHLxiICZIsY";
-        if (jwtTokenProvider.validateToken(token)) {
-            return ResponseEntity.ok("Valid Token");
-        }
-        return ResponseEntity.ok("InValid Token");
-    }
 
     @PostMapping("/api/flight/details")
     public ResponseEntity<?> getFlightDetails(@RequestHeader("Authorization") String token, @RequestBody FlightDetails request) {
-        if (jwtTokenProvider.validateToken(token)) {
-            FlightValues flightDetails = flightService.getFlightDetails(request);
-            if (flightDetails == null) {
-                return new ResponseEntity<>("No details Found", HttpStatus.OK);
+        if (rateLimiter.tryAcquire()) {
+            if (jwtTokenProvider.validateToken(token)) {
+                FlightValues flightDetails = flightService.getFlightDetails(request);
+                if (flightDetails == null) {
+                    return new ResponseEntity<>("No details Found", HttpStatus.OK);
+                }
+                return new ResponseEntity<>(flightDetails, HttpStatus.OK);
             }
-            return new ResponseEntity<>(flightDetails, HttpStatus.OK);
+            return ResponseEntity.ok("InValid Token");
+        } else {
+            return new ResponseEntity<>("Number of requests exceeded", HttpStatus.OK);
         }
-        return ResponseEntity.ok("InValid Token");
     }
 
     @PostMapping("/api/flight/book")
     public ResponseEntity<?> reserveFlight(@RequestHeader("Authorization") String token, @RequestBody FlightReservationRequest flightReservationRequest) {
-        if (jwtTokenProvider.validateToken(token)) {
-            return flightService.bookTicket(flightReservationRequest);
+        if (rateLimiter.tryAcquire()) {
+            if (jwtTokenProvider.validateToken(token)) {
+                return flightService.bookTicket(flightReservationRequest);
+            }
+            return ResponseEntity.ok("InValid Token");
+        } else {
+            return new ResponseEntity<>("Number of requests exceeded", HttpStatus.OK);
         }
-        return ResponseEntity.ok("InValid Token");
+
     }
 
 
     @GetMapping("/api/flight/retrieve")
-    public ResponseEntity<?> findFlightBookingDetails(@RequestHeader("Authorization") String token, @RequestParam("pnr") String pnr){
-        if (jwtTokenProvider.validateToken(token)) {
-            FlightReservationRequest flightReservationRequest = flightService.getFlightBookingDetails(pnr);
-            if(flightReservationRequest == null){
-                return new ResponseEntity<>("Invalid PNR", HttpStatus.OK);
+    public ResponseEntity<?> findFlightBookingDetails(@RequestHeader("Authorization") String token, @RequestParam("pnr") String pnr) {
+        if (rateLimiter.tryAcquire()) {
+            if (jwtTokenProvider.validateToken(token)) {
+                FlightReservationRequest flightReservationRequest = flightService.getFlightBookingDetails(pnr);
+                if (flightReservationRequest == null) {
+                    return new ResponseEntity<>("Invalid PNR", HttpStatus.OK);
+                }
+                return new ResponseEntity<>(flightReservationRequest, HttpStatus.OK);
             }
-            return new ResponseEntity<>(flightReservationRequest, HttpStatus.OK);
+            return ResponseEntity.ok("InValid Token");
+        } else {
+            return new ResponseEntity<>("Number of requests exceeded", HttpStatus.OK);
         }
-        return ResponseEntity.ok("InValid Token");
     }
 }
---
+++ akasaair/src/main/java/com/example/akasaair/utilities/RateLimiterConfig.java
@@ -0,0 +1,16 @@
+package com.example.akasaair.utilities;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public RateLimiter rateLimiter() {
+        return RateLimiter.create(5.0);
+    }
+
+}
+
